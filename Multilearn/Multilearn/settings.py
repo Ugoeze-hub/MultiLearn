46db50a24319a0752d673189a66cfc142bef3429
Cyber Coder <adewunmiazeez226@gmail.com>
2025-07-10 22:36:34 +0100

    fixed the settings

--- Multilearn/Multilearn/settings.py
+++ Multilearn/Multilearn/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
         'PORT': 5432,
         # 'OPTIONS': dict(parse_qsl(tmpPostgres.query)),
         'OPTIONS': {
-             'sslmode': 'require'  # Required for Neon
+            'sslmode': 'require'  # Required for Neon
         }       
     }
 }
@@ -134,7 +134,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '../static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
